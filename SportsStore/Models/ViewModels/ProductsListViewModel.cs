acac6394e57913c7864e282151e3b98bf05e5629
EndlessYxunomei <89537852+EndlessYxunomei@users.noreply.github.com>
2024-07-06 14:36:18 +0300

    обновлено на онец 11 главы

--- SportsStore/Models/ViewModels/ProductsListViewModel.cs
+++ SportsStore/Models/ViewModels/ProductsListViewModel.cs
@@ -1,8 +1,9 @@
-﻿namespace SportsStore.Models.ViewModels
-{
-	public class ProductsListViewModel
-	{
-		public IEnumerable<Product> Products { get; set; } = Enumerable.Empty<Product>();
-		public PagingInfo PagingInfo { get; set; } = new();
-	}
+﻿namespace SportsStore.Models.ViewModels {
+
+    public class ProductsListViewModel {
+        public IEnumerable<Product> Products { get; set; }
+            = Enumerable.Empty<Product>();
+        public PagingInfo PagingInfo { get; set; } = new();
+        public string? CurrentCategory { get; set; }
+    }
 }
